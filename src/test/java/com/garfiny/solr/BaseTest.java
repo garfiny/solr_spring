e123d8a744924f0ab5545682e8b6ff6b6dc52218
Shuo <shuo@>
2014-05-22 11:12:13 +1000

    added guava

--- src/test/java/com/garfiny/solr/BaseTest.java
+++ src/test/java/com/garfiny/solr/BaseTest.java
@@ -4,16 +4,17 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {Application.class})
-public class BaseTest {
+public class BaseTest extends AbstractJUnit4SpringContextTests {
 
-	@Autowired
-    private ApplicationContext applicationContext;
-	
-	public ApplicationContext getApplicationContext() {
-		return applicationContext;
-	}
+//	@Autowired
+//    private ApplicationContext applicationContext;
+//	
+//	public ApplicationContext getApplicationContext() {
+//		return applicationContext;
+//	}
 }
