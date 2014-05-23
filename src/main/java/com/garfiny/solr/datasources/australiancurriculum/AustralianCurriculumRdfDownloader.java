5a2ff05a60d0095b6fa462cc0075adee65189765
Shuo <shuo@>
2014-05-23 16:48:09 +1000

    fetch root english rdf and fetch json TOC

--- src/main/java/com/garfiny/solr/datasources/australiancurriculum/AustralianCurriculumRdfDownloader.java
+++ src/main/java/com/garfiny/solr/datasources/australiancurriculum/AustralianCurriculumRdfDownloader.java
@@ -1,20 +1,20 @@
 package com.garfiny.solr.datasources.australiancurriculum;
 
+import static java.nio.file.StandardOpenOption.CREATE;
+
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.StringWriter;
-import java.io.Writer;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import static java.nio.file.StandardOpenOption.*;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
@@ -22,6 +22,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.NodeIterator;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.ResIterator;
+import com.hp.hpl.jena.rdf.model.SimpleSelector;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.DC_11;
+import com.hp.hpl.jena.vocabulary.DCTerms;
 
 @Component
 @PropertySource("classpath:australian_curriculum_rdf.properties")
@@ -40,15 +56,71 @@ public class AustralianCurriculumRdfDownloader {
 
 	@Value("${download_target_dir}")
 	private Resource downloadTargetDir;
+	
+	@Value("${english_resource_uri}")
+	private Resource englishResourceUri;
 
-	public Resource getAccSparqlUrl() {
-		return accSparqlUrl;
-	}
-
-	public Resource getEnglishQuery() {
-		return englishQuery;
+	public void downloadEnglish() {
+		try (InputStream is = createConnection().getInputStream()) {
+			Model model = ModelFactory.createDefaultModel();
+			model.read(is, null);
+			saveToDisk(model);
+			standardDocument(model);
+		}catch(URISyntaxException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	private void standardDocument(Model model) {
+		com.hp.hpl.jena.rdf.model.Resource resource = model.getResource(englishResourceUri.toString());
+		Property toc = model.getProperty(DCTerms.tableOfContents.toString());
+		Statement stmt = model.getRequiredProperty(null, toc);
+		System.out.println(stmt.getObject().toString());
+		donwloadTableOfContents(stmt.getObject().toString());
+//		StmtIterator itr = model.listStatements(new SimpleSelector(resource, null, (RDFNode)null));
+//		StmtIterator itr = model.listStatements(new SimpleSelector(null, toc, (RDFNode)null));
+//		int i = 0;
+//		while (itr.hasNext()) {
+//			i++;
+//		    Statement stmt      = itr.nextStatement();  // get next statement
+//		    printStatement(stmt);
+//		}
+//		System.out.println("Total statements: " + i);
+	}
+	
+	private void donwloadTableOfContents(String resourceUrl) {
+		try (BufferedReader reader = 
+				new BufferedReader(new InputStreamReader(
+						new URL(resourceUrl).openStream()))) {
+//			JsonReader jsonReader = new JsonReader(reader);
+			IOUtils.copy(reader, System.out);
+		} catch(IOException ix) {
+			ix.printStackTrace();
+		}
+	}
+	
+	private void printStatement(Statement stmt) {
+		com.hp.hpl.jena.rdf.model.Resource subject = stmt.getSubject(); // subject
+		Property predicate = stmt.getPredicate(); // get the predicate
+		RDFNode object = stmt.getObject(); // get the object
+		System.out.print(subject.toString());
+		System.out.print(" == " + predicate.toString() + " == ");
+		if (object instanceof Resource) {
+			System.out.print(object.toString());
+		} else {
+			System.out.print(" \"" + object.toString() + "\"");
+		}
+		System.out.println(" .");
+	}
+	
+	private URLConnection createConnection() throws IOException, URISyntaxException {
+		URI uri = new URI(null, null, accSparqlUrl.getURI().toString(),	
+				"query=" + sparqlQueryToString(englishQuery), null);
+		URLConnection connection = uri.toURL().openConnection();
+		connection.setRequestProperty("Accept", "application/rdf+xml");
+		return connection;
+	}
+
 	private String sparqlQueryToString(Resource queryFile) {
 		try(InputStream is = queryFile.getInputStream()) {
 			StringWriter writer = new StringWriter();
@@ -58,31 +130,22 @@ public class AustralianCurriculumRdfDownloader {
 			return "";
 		}
 	}
+	
+	private void saveToDisk(Model model) throws IOException {
+		Path path = Paths.get(downloadTargetDir.getURI());
+		Path file = path.resolve(ENGLISH_RDF_FILENAME);
+		Files.deleteIfExists(file);
+		try (BufferedWriter writer = 
+				Files.newBufferedWriter(Files.createFile(file), CREATE)) {
+			model.write(writer);
+		}catch(IOException ix) { throw ix;}
+	}
+	
+	public Resource getAccSparqlUrl() {
+		return accSparqlUrl;
+	}
 
-	public void downloadEnglish() {
-		InputStream inputStream = null;
-		BufferedWriter writer = null;
-		try {
-			URI uri = new URI(null, null, accSparqlUrl.getURI().toString(),
-					"query=" + sparqlQueryToString(englishQuery), null);
-			URLConnection connection = uri.toURL().openConnection();
-			connection.setRequestProperty("Accept", "application/rdf+xml");
-			
-			Path path = Paths.get(downloadTargetDir.getURI());
-			Path file = path.resolve(ENGLISH_RDF_FILENAME);
-			Files.deleteIfExists(file);
-			writer = Files.newBufferedWriter(Files.createFile(file), CREATE);
-			inputStream = connection.getInputStream();
-			IOUtils.copy(inputStream, writer, "UTF-8");
-		} catch (URISyntaxException | IOException e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				inputStream.close();
-				writer.close();
-			}catch(IOException ix) {
-				ix.printStackTrace();
-			}
-		}
+	public Resource getEnglishQuery() {
+		return englishQuery;
 	}
 }
